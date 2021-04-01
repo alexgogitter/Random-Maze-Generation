8bebe597f5b9223395b3af4c6d6a52902a24fb12
alexgogitter <60302170+alexgogitter@users.noreply.github.com>
2021-04-01 11:14:59 +0100

    Add files via upload

--- MazeGenAlgorithm.py
+++ MazeGenAlgorithm.py
@@ -1,46 +1,168 @@
 ##Maze Generation
+import pygame
 
 print("importing Maze Generation Version 0")
 
 
 class node:
-    def __init__(self, position):
+    def __init__(self, position, width,):
+
         self.parent = None
+
+        self.width = width
+
+        self.neighbours = []
+
         self.position = position
-        self.traversed = False
-        self.nodeDimension = 8
-        traversableCol = (255, 255, 255)
-        nonTraversableCol = (64, 64, 64)
 
-    def draw(screen):
-        if traversed:
-            pygame.draw.rect(screen, traversableCol, (position[0], position[1], 8, 8), 0)
-        else:
-            pygame.draw.rect(screen, nonTraversableCol, (position[0], position[1], 8, 8), 0)
+        self.traversable = False
+
+        self.visited = False
+
+        self.edge = False
+
+        self.start = False
+
+        self.rectang = ((self.position[0]*10) + 1, (self.position[1]*10) + 1, (self.width - 2), (self.width - 2))
+
+        self.traversableCol = (255, 255, 255)
+
+        self.nonTraversableCol = (64, 64, 64)
+
+    def drawNeighbours(self, screen,):
+        for neighbour in self.neighbours:
+            pygame.draw.rect(screen, (255, 255, 255,), neighbour.rectang, 0,)
+
+    def drawNode(self, screen):
 
 
+        if self.start:
+    
+            pygame.draw.rect(screen, (32, 255, 32), self.rectang, 0,)
+
+        elif self.traversable:
+            
+            pygame.draw.rect(screen, self.traversableCol, self.rectang, 0,)
+
+        elif not self.traversable and not self.start:
+
+            pygame.draw.rect(screen, self.nonTraversableCol, self.rectang, 0,)
+
+    
+
+    def setEState(self, state,):
+        self.edge = state
+
+    def setEnd(self, state,):
+        self.end = state
+
+    def setNeighbours(self, listData,):
+        self.neighbours = list(listData)
+
+    def addNeighbours(self, data,):
+
+        self.neighbours.append(data)
+
+    def setStart(self, state,):
+        self.start = state
+
+    def getStartorFinish(self):
+        return self.start
+
+def removeRepeats(data):
+    if len(data) >= 1:
+        
+        return data
+
+    else:
+        for i in data:
+            for j in data:
+                if data[j] == data[i]:
+                    data.remove(data[j])
+
+    return data 
 
 
+def getNeighbour(nodeMatrix):
+    for x in range(len(nodeMatrix)):
+        
+        for y in range(len(nodeMatrix[x])):
 
-def generateMazeBase(windowDimensions, nodeWidth, nodeMatrix, screen,):
+            xIncr = 1
+
+            yIncr = 1
+
+            if x == (0 or (len(nodeMatrix))):
+
+                xIncr = 0
+
+            elif y == (0 or (len(nodeMatrix[x]))):
+
+                yIncr = 0
+
+            possibleNeighbours = [
+
+                ((x - xIncr), y),
+
+                ((x + xIncr), y),
+
+                (x, (y - yIncr)),
+
+                (x, (y + yIncr))
+
+            ]
+            
+            possibleNeighbours = removeRepeats(possibleNeighbours)
+
+            for position in possibleNeighbours:
+
+                ##print((position, y, x, xIncr, yIncr))
+
+                try:
+                
+                    nodeMatrix[x][y].addNeighbours(nodeMatrix[position[0]][position[1]])
+                
+                except IndexError:
+                    continue
+
+def generateMazeBase(windowDimensions, nodeWidth, edges,):
+
     navWidth = windowDimensions[0]//nodeWidth
+
     navHeight = windowDimensions[1]//nodeWidth
 
     xList = []
     yList = []
 
     for xNode in range(0, (navWidth + 1), 1):
+
         for yNode in range(0, (navHeight + 1), 1):
+
             position = [xNode, yNode]
-            Node = node(position)
-            xList.append(Node)
+
+            Node = node(position, nodeWidth)
+
+            if xNode == 0 or yNode == 0:
+
+                Node.setEState(True)
+
+                edges.append(Node)
+
+            elif xNode + 1 == 100 or yNode + 1 == 70:
+                Node.setEState(True)
+
+                edges.append(Node)
+
+            yList.append(Node)
 
         xList.append(list(yList))
-        yList.clear
 
-    nodeMatrix.append(list(xList))
+        yList.clear()
 
+    nodeMatrix = xList
 
+    getNeighbour(nodeMatrix)
 
-
+    return nodeMatrix
+    
 
--- RandomMazeGeneration.py
+++ RandomMazeGeneration.py
@@ -1,33 +1,77 @@
-import pygame
-import MazeGenAlgorithm
 import sys
+
+import pygame
+
+import MazeGenAlgorithm
+
+import random
 
 pygame.init()
 
-windowDimensions = (900, 600)
-bgColour = (32, 32, 32)
+windowDimensions = (500, 500)
+
+bgColour = (8, 8, 8)
+
+edges = []
+
+openList = []
+
+closedList = []
 
 displayName = ("Random Maze Generation Version 0")
 
 displaySurface = pygame.display.set_mode(windowDimensions)
-pygame.display.set_caption(displayName)
-displaySurface.fill(bgColour)
 
-pygame.display.flip()
+pygame.display.set_caption(displayName)
 
 running = True
 
-nodeMatrix = []
+nodeMatrix = MazeGenAlgorithm.generateMazeBase(windowDimensions, 10, edges,)
 
-MazeGenAlgorithm.generateMazeBase(windowDimensions, 10, nodeMatrix, displaySurface)
+def drawNodes(nodeMat):
 
-print(nodeMatrix)
+    matrix = nodeMat
+
+    for x in matrix:
+
+        for y in x:
+
+            y.drawNode(displaySurface)
+
+def randomPrimsAlgorithm():
+
+
+
+    pass
+
+def randEdge():
+
+    mazeEdgeNode = random.choice(edges)
+
+    mazeEdgeNode.setStart(True)
+
+    mazeEdgeNode.setEState(False)
+
+    edges.remove(mazeEdgeNode)
+
+    return mazeEdgeNode
+
+mazeStart = randEdge()
+
+currentNode = mazeStart
 
 while running:
+
     for event in pygame.event.get():
+
         if event.type == pygame.QUIT:
+
             pygame.quit()
+
             sys.exit()
 
+    displaySurface.fill(bgColour)
 
-
+    drawNodes(nodeMatrix)
+    
+    pygame.display.flip()
